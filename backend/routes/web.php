756d7db14858c3c8e1589d344af6e8c8245698e5
tomoya110404 <moonlight.02.1104@gmail.com>
2021-05-20 18:41:37 +0900

    add: hello wolrd表示まで

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -12,9 +12,12 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
-Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+    
+
+
+Auth::routes();
